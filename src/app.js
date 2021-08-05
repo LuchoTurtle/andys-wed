9be254afc93fa750e59480a36a528c0a74dec5a9
Luis <lucho14garfield@gmail.com>
2021-08-05 02:02:42 +0100

    :heavy_plus_sign: :recycle: Adding GSAP and dabblign Locomotive Scroll

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import './style.scss'
 import './locomotive.css'
 import LocomotiveScroll from 'locomotive-scroll';
+import { gsap } from "gsap";
 
 const scroller = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),

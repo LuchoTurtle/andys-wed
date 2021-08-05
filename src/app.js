b0b7b21e14724c9efb94038aec1bef8423876481
Luis <lucho14garfield@gmail.com>
2021-08-05 13:27:33 +0100

    :bug: Locomotive now updates GSAP.

--- src/app.js
+++ src/app.js
@@ -2,8 +2,48 @@ import './style.scss'
 import './locomotive.css'
 import LocomotiveScroll from 'locomotive-scroll';
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-const scroller = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
+gsap.registerPlugin(ScrollTrigger);
+
+/* Progress bar */
+const locoScroll = new LocomotiveScroll({
+    el: document.querySelector(".smooth-scroll"),
     smooth: true
 });
+locoScroll.on("scroll", ScrollTrigger.update);
+ScrollTrigger.scrollerProxy(".smooth-scroll", {
+    scrollTop(value) {
+        return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+    },
+    getBoundingClientRect() {
+        return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+    },
+    pinType: document.querySelector(".smooth-scroll").style.transform ? "transform" : "fixed"
+});
+
+
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+ScrollTrigger.refresh();
+
+
+// --- RED PANEL ---
+gsap.to('progress', {
+    value: 100,
+    ease: 'none',
+    scrollTrigger: {
+        scroller: ".smooth-scroll",
+        markers: true,
+        scrub: 0.3,
+        start: "top top",
+        end: "bottom bottom"
+    }
+});
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+

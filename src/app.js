7c25e43a77db789ffdfca1198afece700bb60cd4
Luis <lucho14garfield@gmail.com>
2021-08-06 12:58:07 +0100

    :sparkles: Sidebar with Loco

--- src/app.js
+++ src/app.js
@@ -3,14 +3,17 @@ import './locomotive.css'
 import LocomotiveScroll from 'locomotive-scroll';
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-
 gsap.registerPlugin(ScrollTrigger);
 
-/* Progress bar */
+
+const progress_bar = document.querySelector('.progress-bar')
+
 const locoScroll = new LocomotiveScroll({
     el: document.querySelector(".smooth-scroll"),
     smooth: true
 });
+
+/* Linking GSAP with Locomotive Scroll - making sure they update accordingly --------------------------------- */
 locoScroll.on("scroll", ScrollTrigger.update);
 ScrollTrigger.scrollerProxy(".smooth-scroll", {
     scrollTop(value) {
@@ -21,29 +24,33 @@ ScrollTrigger.scrollerProxy(".smooth-scroll", {
     },
     pinType: document.querySelector(".smooth-scroll").style.transform ? "transform" : "fixed"
 });
-
-
-ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-ScrollTrigger.refresh();
-
-
-// --- RED PANEL ---
-gsap.to('progress', {
-    value: 100,
-    ease: 'none',
-    scrollTrigger: {
-        scroller: ".smooth-scroll",
-        markers: true,
-        scrub: 0.3,
-        start: "top top",
-        end: "bottom bottom"
-    }
-});
-
 // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
 ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
 // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
 ScrollTrigger.refresh();
 
+/* --------------------------------- */
+
+/* Progress Bar */
+locoScroll.on('scroll', ({ limit, scroll }) => {
+    const progress = 100 - (scroll.y / limit.y * 100)
+    progress_bar.style.transform = `translateY(-${progress}%)`
+})
+
+
+/*
+// --- RED PANEL ---
+gsap.to('progress', {
+    value: 100,
+    ease: 'none',
+    scrollTrigger: {
+        scroller: ".smooth-scroll",
+        //markers: true,
+        scrub: 0.3,
+        start: "top top",
+        end: "bottom bottom"
+    }
+});
+ */
 

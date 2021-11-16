3beb195ced985d758ece0825386f02d7121e4cac
Lucho <lucho14garfield@gmail.com>
2021-11-16 21:42:28 +0000

    :egg: Adding console log easter egg #3

--- src/app.js
+++ src/app.js
@@ -205,3 +205,30 @@ const tick = () => {
 };
 
 tick();
+
+
+/** -------------------------------- CONSOLE LOG EASTER EGG ---------------------------------------------- **/
+
+
+let title_style = [
+    'font-weight: bold',
+    'font-size: 50px',
+    `color: red`,
+    `text-shadow: 3px 3px 0 rgb(217,31,38)`,
+    `6px 6px 0 rgb(226,91,14)`,
+    `9px 9px 0 rgb(245,221,8)`,
+    `12px 12px 0 rgb(5,148,68)`,
+    `15px 15px 0 rgb(25,18,168)`
+].join(';');
+console.log('%c Hey there!', title_style);
+
+let subtitle_styles = [
+    `font-size: 12px`,
+    `font-family: monospace`,
+    `background: white`,
+    `display: inline-block`,
+    `color: black`,
+    `padding: 8px 19px`,
+    `border: 1px dashed;`
+].join(';');
+console.log("%c Great seeing you here 😊.\nWe always fancy those that are curious by nature. But today it's time to celebrate. Head on back to the website and get ready for the best wedding ever! 🎉", subtitle_styles);
